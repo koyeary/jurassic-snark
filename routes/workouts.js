6426caa692892ddd441056d99740c2de382ac0b1
koyeary <katyeary@gmail.com>
2020-09-23 18:26:09 -0700

    Update comments

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,84 +1,8 @@
-/* const express = require('express')
-const router = express.Router()
-const Workout = require('../models/Workout')
-
-// Get all
-router.get('/workouts', async (req, res) => {
-  try {
-    const workouts = await Workout.find()
-    res.json(workouts)
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
-
-// Get Range
-router.get('/workouts/range', getWorkout, (req, res) => {
-  res.json(res.workout
-})
-
-// Create one
-router.post('/workouts', async (req, res) => {
-  const workout = new Workout({
-    workout: req.body.name,
-    subscribedToChannel: req.body.subscribedToChannel
-  })
-  try {
-    const newWorkout = await workout.save()
-    res.status(201).json(newWorkout)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
-
-// Update One
-router.patch('/workouts/:id', getWorkout, async (req, res) => {
-  if (req.body.name != null) {
-    res.workout.name = req.body.name
-  }
-  if (req.body.subscribedToChannel != null) {
-    res.workout.subscribedToChannel = req.body.subscribedToChannel
-  }
-  try {
-    const updatedWorkout = await res.workout.save()
-    res.json(updatedWorkout)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
-
-// Delete One
-router.delete('/workouts/:id', getWorkout, async (req, res) => {
-  try {
-    await res.workout.remove()
-    res.json({ message: 'Deleted Workout' })
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
-})
-
-async function getWorkout(req, res, next) {
-  let workout
-  try {
-    workout = await Workout.findById(req.params.id)
-    if (workout == null) {
-      return res.status(404).json({ message: 'Cannot find workout' })
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message })
-  }
-
-  res.workout = workout
-  next()
-}
-
-module.exports = router */
-
 const express = require('express');
 const router = express.Router();
 const Workout = require('../models/Workout');
 
-// Get all
+// Get All
 router.get('/', async (req, res) => {
   try {
     const workouts = await Workout.find();
@@ -120,6 +44,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Update One
 router.put("/:id", async (req, res) => {
   try {
   const workout = await Workout.findByIdAndUpdate(req.params.id,
@@ -150,18 +75,5 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-/* async function getWorkout(req, res, next) {
-  let workout;
-  try {
-    workout = await Workout.findById(req.params.id);
-    if (workout == null) {
-      return res.status(404).json({ message: 'Cannot find workout' });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-  res.workout = workout;
-  next();
-} */
 
-module.exports = router
+module.exports = router;
