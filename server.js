15d055902d7ef88d1558634eef9316c6e47b9d81
koyeary <katyeary@gmail.com>
2020-10-03 19:24:38 -0700

    Migrate from broken version complete. Add JSON virtuals

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-require('dotenv').config();
-
 const express = require('express');
-const logger = require('morgan');
+/* const logger = require('morgan'); */
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
-app.use(logger('dev'));
+/* app.use(logger('dev')); */
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/fitnesstracker', {
+mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
+
+//
+mongoose.set('toJSON', { virtuals: true });
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database'));
 
-app.use(require('./routes/workouts'));
-app.use(require('./routes/routes'));
+app.use(require('./routes/api.js'));
+app.use(require('./routes/routes.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
